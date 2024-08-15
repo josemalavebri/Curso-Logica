9d05120eac0230047bfa42c7b7fb768174395a26
Jose <165485943+josemalavebri@users.noreply.github.com>
2024-08-15 14:06:00 -0500

    subiendo ejemplo

--- Clases/Ejemplos/Clase 4.cs
+++ Clases/Ejemplos/Clase 4.cs
@@ -1,42 +1,81 @@
-int variable1;
 
-void datos(){
-    int variable2;
-    Console.WriteLine(variable2);
+//Introduccion a funciones
+int a = 12;
+int b = 20;
 
-    Console.WriteLine(variable1);
-
-    if(condicion){
-        int variable3;
-        Console.WriteLine(variable3);
-        Console.WriteLine(variable1);
-
-    }
-    Console.WriteLine(variable3);
-}
-    Console.WriteLine(variable)
-
-
-
-
-Console.WriteLine("La división es: " + Dividir(num1, num2));
-
-
-Console.WriteLine("La suma es: " + Multiplicar(num1, num2));
-Console.WriteLine("La multiplicación es: " +Sumar(num1, num2) );
-
-Console.WriteLine("Ingrese el primer número:");
-double num1 = Convert.ToDouble(Console.ReadLine());
-
-double Multiplicar(double a, double b)
-{
-    return a * b;
+void sumar(int a, int b){
+    int suma = a+b;
+    Console.WriteLine(suma);
 }
 
-Console.WriteLine("Ingrese el segundo número:");
-double num2 = Convert.ToDouble(Console.ReadLine());
+//sumar(a,b);
 
 
+//Tipos de funciones
+
+//tipo de funcion sin retorno
+void funcionSinRetorno(int a, int b){
+    int suma = a+b;
+    Console.WriteLine(suma);
+}
+
+//tipo de funcion que retorna algo
+int funcionRetorno(int a, int b){
+    int suma = a+b;
+    return suma;
+}
+
+
+//tipo de funcion que retorna algo
+int funcionSinParametro(){
+    int suma = 10+20;
+    return suma;
+}
+
+
+//tipo de funcion que retorna algo
+int funcionConParametro(int a, int b){
+    int suma = a+b;
+    return suma;
+}
+//-------------------------//-------------------------
+
+//Funciones que alteran estados
+//estado
+int entero = 0;
+
+void funcionAlteraEstado(int valor){
+     entero = valor;
+     Console.WriteLine(entero);
+}
+
+void sumario(int valor){
+     int resultado = entero + valor;
+     Console.WriteLine(resultado);
+     if(condicion){
+        int resultado2 = 0;
+        Console.WriteLine(resultado2);
+     }
+
+
+}
+//-------------------------//-------------------------
+
+
+//Modularidad de codigo
+int resultado = entero + 10;
+     Console.WriteLine(resultado);
+
+int resultado2 = entero + 11;
+     Console.WriteLine(resultado2);
+
+sumario(10);
+sumario(11);
+sumario(12);
+sumario(13);
+sumario(14);
+
+funcionAlteraEstado(11);
 
 
     
