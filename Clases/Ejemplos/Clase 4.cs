fa660d1acc712ed40280cb42b3958dd1f73e5001
Jose <165485943+josemalavebri@users.noreply.github.com>
2024-08-15 11:06:43 -0500

    tareas

---
+++ Clases/Ejemplos/Clase 4.cs
@@ -0,0 +1,42 @@
+int variable1;
+
+void datos(){
+    int variable2;
+    Console.WriteLine(variable2);
+
+    Console.WriteLine(variable1);
+
+    if(condicion){
+        int variable3;
+        Console.WriteLine(variable3);
+        Console.WriteLine(variable1);
+
+    }
+    Console.WriteLine(variable3);
+}
+    Console.WriteLine(variable)
+
+
+
+
+Console.WriteLine("La división es: " + Dividir(num1, num2));
+
+
+Console.WriteLine("La suma es: " + Multiplicar(num1, num2));
+Console.WriteLine("La multiplicación es: " +Sumar(num1, num2) );
+
+Console.WriteLine("Ingrese el primer número:");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+double Multiplicar(double a, double b)
+{
+    return a * b;
+}
+
+Console.WriteLine("Ingrese el segundo número:");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+
+
+
+    
