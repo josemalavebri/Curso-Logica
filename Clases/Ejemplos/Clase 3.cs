e1002f29f8ac2e5bce769a734657072d244656a5
Jose <165485943+josemalavebri@users.noreply.github.com>
2024-08-14 20:46:20 -0500

    enviando ejercicios

---
+++ Clases/Ejemplos/Clase 3.cs
@@ -0,0 +1,26 @@
+
+//Numericos enteros
+int entero = 1;
+int entero2 = 2;
+byte pocoPeso = 25;
+int pesoIntermedio = 25612121;
+long muchopeso = 256122121212121;
+
+//Numericos Decimales
+float pocosDecimales = 2.21121f;
+double muchosDecimales = 2.2112121212d;
+
+//Operaciones con numericos
+int suma = entero + entero2;
+
+//Especiales
+char caracter = 'A';
+string cadena2 = "hola como estas ";
+bool bandera = true;
+
+//Operaciones con cadenas
+string resultado =  "mi perro"+" - "+ "sale a pasiar" ;
+
+
+//Imprimir datos por consola
+Console.WriteLine(bandera);
