12d98d37bdca27478f58428fae8fdf54494095a6
Jose <165485943+josemalavebri@users.noreply.github.com>
2024-08-21 12:44:53 -0500

    añadiendo clase 7

--- Clases/Ejemplos/Clase 7.cs
+++ Clases/Ejemplos/Clase 7.cs
@@ -1,11 +1,14 @@
-/*
+
+
+//ejemplo for con variable externa
 int i=10;
 
-/*
-for(int j=0; j <=5 ; j++){
+for(i=0; j <=5 ; j++){
     Console.WriteLine(j);
 }
 
+
+//distantas ejecuciones de for con operadores
 for(int j=0; j <=6 ; j++){
     Console.WriteLine(j);
 }
@@ -17,24 +20,14 @@ for(int j=1; j <=4 ; j++){
 for(int j=0; j <6 ; j++){
     Console.WriteLine(j);
 }
-*/
-using System.Security.Cryptography;
-
-int c = 0;
-
-/*
-
 
 
 for( j=5; j >= 0 ; c--){
     Console.WriteLine(j);
 }
 
+//practicando los for
 
-int j = 0;
-for( j=7; j >= 5 ; j--){
-    Console.WriteLine(j);
-}
 //1 hasta el 3
 int a = 1;
 for(; a <4 ;a++){
@@ -42,15 +35,13 @@ for(; a <4 ;a++){
 }
 
 //3 hasta el 1
-/*
+
 for(int b = 3 ; b >= 1 ; b--){
     Console.WriteLine(b);
 }
 */
 
-/*
 
-/*
 //Enseñar variables por fuera y por dentro
 
 int numeroParcial(){
@@ -60,37 +51,44 @@ int numeroParcial(){
 string cadena = "hola";
 
 void metodoVacio(){
-
-    for(int numero = 0 ; numero <= 1 ; numero--){
+    for(int numero = numeroParcial(); numero <= 1 ; numero--){
         Console.WriteLine(numero);
     }
 
 }
 
+
+
+//Operaciones dinamicas
+void metodoVacio(){
+    string cadena = "hola";
+
+    for(int numero = cadena.length ; numero <= 1 ; numero--){
+        Console.WriteLine(numero);
+    }
+
+}
+
+//Ejemplo de while
 int numeroContador = 0;
 
 while (numeroContador <= 10) {
     Console.WriteLine(numeroContador);
     numeroContador++;
 }
-/*
-
-*/
 
 
+
+//While recursivo
 int numeroContador2(int numero){
     return numero--;
 }
 
 int numero = 4;
 
-while (numero >= 5) {
+while (numero >= (numeroContador2(numero))) {
     
     Console.WriteLine(numeroContador2);
     numero++;
 
 }
-
-
-
-
