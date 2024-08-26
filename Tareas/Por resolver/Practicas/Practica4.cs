4c9b85a15da2a1094be7f74f7f7a4628693ed27e
Jose <165485943+josemalavebri@users.noreply.github.com>
2024-08-26 09:11:04 -0500

    enviando datos

---
+++ Tareas/Por resolver/Practicas/Practica4.cs
@@ -0,0 +1,218 @@
+
+
+bool pelotaMaria = false; //x
+bool pelotaFalquez = true; //x
+bool pelotaReyes = true; //x
+
+
+int metodo(){
+    return 0;
+}
+
+char metodo1(){
+    char a = 'a';
+    return a;
+}
+
+bool metodo2(){
+    bool a = true;
+    return a;
+}
+
+
+/*
+char metodo1(){
+    char a = "a';
+    return a;
+}
+
+*/
+
+
+/*
+void metodo4(){
+    return 0;
+}
+*/
+
+/*
+string hola "hola";
+
+int numero = 09999;
+
+bool false = false;
+
+
+string hola = "hola";
+
+int numero = 1;
+
+bool bandera  = false;
+
+/*
+
+int entero(){
+    return numero();
+}
+
+bool entero2(){
+    return true;
+}
+
+*/
+
+/*
+
+int numero corto = 1000;
+
+int numerolargo= 10;
+
+bool bandera = false;
+
+string cadena = "c"
+*/
+
+/*
+int numeroCorto = 1000;
+
+int numeroLargo = 10;
+
+bool bandera = false;
+
+/*
+
+string cadenaLarga = "hola como estas";
+
+bool bandera = metodo();
+
+int campoVacio = 0;
+
+double numerote = 12;
+
+*/
+
+
+/*
+
+int void(){
+    return metodo(1);
+}
+
+int metodo(int i){
+    return 1+i;
+}
+
+*/
+
+int metodoexterno(){
+    return metodo();
+}
+
+int salida2(int i){
+    return 1+i;
+}
+
+
+/*
+
+int metodoEntrada2(){
+    return 12;
+}
+
+int metodoEntrada3(){
+    return 15;
+}
+
+*/
+
+/*
+
+bool metodoEntrada4(){
+    return metodoEntrada5(false);
+}
+
+bool metodoEntrada5(){
+    return false;
+}
+
+*/
+
+/*
+bool metodoEntrada4(){
+    bool bandera = metodoEntrada5(false);
+    return bandera;
+}
+
+bool metodoEntrada5(bool entrada){
+    return false;
+}
+
+int resultado = metodoProceso();
+
+int metodoProceso(){
+    int a = 2+2;
+    return a;
+}
+*/
+
+//Crear una variable de tipo entero y
+// asignale un metodo que retorne a resta de 5 y 4
+
+/*
+int entero = 0;
+entero = 5-4;
+return entero;
+
+
+int resta1 = calculoresta();
+Console.WriteLine(resta1);
+
+int calculoresta(){
+    int calculo1 = 5;
+    int calculo2 = 4;
+    return calculo1-calculo2;
+}
+*/
+
+//Crear una variable de tipo double y
+// asignale un metodo que retorne a resta de 3.1 y 2.0
+
+
+double resta1 = calculoresta();
+
+double metodocalcular(){
+     return calculoresta();
+}   
+
+double calculoresta(){
+    double calculo1 = 3.1;
+    double calculo2 = 2.0;
+    return 3.1-2.0;
+}
+
+
+//Crear una variable de tipo int  y
+// asignale un proceso que retorne una multiplicacion de 3 y 2
+
+
+int entero = multiplicacion();
+int multiplicacion(){
+    return 3*2;
+} 
+
+
+
+
+void metodo() {
+        Console.WriteLine("Ingresa el primer número: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ingresa el segundo número: ");
+        int num2 = Convert.ToInt32(Console.ReadLine())
+
+        Console.WriteLine("Ingresa el tercer número: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        double promedio = (num1 + num2 + num3) / 2;  
+        Console.WriteLine("El promedio de los tres números es: " + promedio);
+ }
