462aa4a889b86447e1d30e7784d50db907da7932
Jose <165485943+josemalavebri@users.noreply.github.com>
2024-08-27 13:07:04 -0500

    practica5

---
+++ Clases/Ejemplos/Clase 8.cs
@@ -0,0 +1,38 @@
+
+
+
+//Sintaxis básica
+TipoDatos[] variableUso;
+
+variableUso = new TipoDatos[CantidadElementos];
+
+
+//Inicializacion en una linea 
+
+int[] edades = new int[4]; //Array de tamaño 4
+
+//Asignacion de elementos
+edades[0] = 0;
+edades[1] = 10;
+edades[2] = 20;
+edades[3] = 30;
+edades[4] = 30;     
+
+
+string[] nombres = new string[10];
+
+
+//Inicializacion en dos lineas 
+string[] nombres;
+nombres = new string[10];
+
+for(int indice = 0; indice<=9; indice++){
+    nombres[indice] = "jose";
+}
+
+
+int[] edades = { 25, 30, 35 };
+
+
+List<string> nombres = new List<string>();
+nombres.Add("Ana");
